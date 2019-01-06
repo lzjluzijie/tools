6fd2c4e48f9b4a9ce3b7a79096ce1df94afdceea
halulu <i@halu.lu>
2019-01-06 11:14:15 +0800

    deploy commit message with timezone

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
-git config --global user.email circleci-bot@halu.lu
-git config --global user.name circleci-bot
+#!/usr/bin/env bash
+export TZ='Asia/Shanghai'
+git config --global user.email circleci-bot-tools@halu.lu
+git config --global user.name circleci-bot-tools
+
 git clone git@github.com:lzjluzijie/tools.git -b gh-pages gh-pages
 go run tools.go
 cp todo.md gh-pages
 cp README.md gh-pages
+
 cd gh-pages
 git add .
-git commit -s -m "$(date +%s)"
+git commit -s -m "Site updated by circleci: `date +"%Y%m%d-%H:%M:%S"` UTC+8"
 git push origin gh-pages
